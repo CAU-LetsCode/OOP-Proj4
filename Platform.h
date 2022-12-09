f39587d90d98cdb0b298d659e26bb2e1efe8ef46
김도엽 <do901328@cau.ac.kr>
2022-12-09 21:47:53 +0900

    Add : construct Platform header file

--- Platform.h
+++ Platform.h
@@ -1,3 +1,5 @@
+#include "d3dUtility.h"
+
 #define PLATFORMWIDTH 50
 #define PLATFORMHEIGHT 50
 #define LTX(x) x - PLATFORMWIDTH / 2	// x of Left Top point
@@ -9,9 +11,20 @@ private:
 	D3DMATERIAL9 m_mtrl;
 	ID3DXMesh* m_pBoundMesh;
 
-	float x, z;	// left top point of square
+	float x, z;	// center point of box
 	float width, depth, height;
 
-public:
+	void setLocalTransform(const D3DXMATRIX& mLocal);
 
+public:
+    Platform(void);
+    Platform(float iwidth, float iheight, float idepth, D3DXCOLOR color);
+    ~Platform(void);
+    bool create(IDirect3DDevice9* pDevice, float ix, float iz, float iwidth, float iheight, float idepth, D3DXCOLOR color);
+    void destroy(void);
+    void draw(IDirect3DDevice9* pDevice, const D3DXMATRIX& mWorld);
+    void setPosition(float x, float y, float z);
+    float getHeight(void) const;
+    //void adjustPosition(CSphere& ball);   //todo
+    D3DXVECTOR3 getPosition() const;
 };
