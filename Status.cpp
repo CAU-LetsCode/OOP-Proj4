55db19bbd51d3e8ec9c55489b2a5668910d7ac1e
hyun-hyang <jimin3060@naver.com>
2022-12-11 21:07:04 +0900

    Added: Class: Status

---
+++ Status.cpp
@@ -0,0 +1,26 @@
+#include "Status.h"
+#include <algorithm>
+
+using std::find_if;
+using std::exception;
+
+Status::Status() {
+	this->numStage = 0;
+	this->isGameOver = false;
+}
+
+void Status::setStage(unsigned int numStage) {
+	this->numStage = numStage;
+}
+
+unsigned int Status::getStage() const noexcept {
+	return numStage;
+}
+
+
+void Status::setIsGameOver(bool isGameOver) {
+	this->isGameOver = isGameOver;
+}
+bool Status::getIsGameOver() const noexcept {
+	return isGameOver;
+}
